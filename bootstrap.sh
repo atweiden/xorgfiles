ac3cc3f41b707b78cfe33d5e868753af769c782a
Andy Weidenbaum <atweiden@ioiojo.com>
2020-08-11 10:37:23 +1000

    git config sendemail
    see
    - https://git-send-email.io/
    - https://gist.github.com/jasonkarns/4354421

--- bootstrap.sh
+++ bootstrap.sh
@@ -169,7 +169,7 @@ fi
 # -----------------------------------------------------------------------------
 
 sed -i "s#yourname#$name#"         "$HOME/.config/git/config"
-sed -i "s#youremail#$email#"       "$HOME/.config/git/config"
+sed -i "s#youremail#$email#g"      "$HOME/.config/git/config"
 sed -i "s#githubusername#$github#" "$HOME/.config/git/config"
 sed -i "s#yourname#$name#"         "$HOME/.config/hg/hgrc"
 sed -i "s#youremail#$email#"       "$HOME/.config/hg/hgrc"
